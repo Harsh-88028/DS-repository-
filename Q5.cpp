d4d45d7a6db2471018be72e5bfa2760137db0aed
Harsh-88028 <81902272+Harsh-88028@users.noreply.github.com>
2021-05-28 09:46:34 +0530

    Create Q5.cpp

---
+++ Q5.cpp
@@ -0,0 +1,24 @@
+5. Given a full m-ary tree with i internal vertices, Write a Program to find the number of leaf nodes.
+
+#include <iostream>
+using namespace std;
+ 
+// Function to calculate
+// leaf nodes in m-ary tree
+int calcNodes(int N, int I)
+{
+    int result = 0;
+ 
+    result = I * (N - 1) + 1;
+ 
+    return result;
+}
+ 
+int main()
+{
+    int N = 5, I = 2;
+ 
+    cout << "Leaf nodes = " << calcNodes(N, I);
+ 
+    return 0;
+}
