291e55286d655a0dd6ec19e221beadc673094791
Harsh-88028 <81902272+Harsh-88028@users.noreply.github.com>
2021-05-30 11:43:02 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -104,3 +104,13 @@ int main()
     findInOutDegree(adjlist, n);
 }
   
+
+Vertex    In    Out
+
+   0      1      2
+   1      2      1
+   2      2      3
+   3      2      2
+   4      2      2
+   5      2      2
+   6      2      1
